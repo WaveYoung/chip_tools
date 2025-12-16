80a0e81da1aba5c9f99729052eea0ed4035b9179
peachyoung <peachyoung@localhost.localdomain>
2025-12-16 14:57:23 +0800

    update build env and agent

--- testbench_lib/config/filelist/vrf.f
+++ testbench_lib/config/filelist/vrf.f
@@ -1,5 +1,7 @@
 +incdir+${BENCH_PATH}
 +incdir+${COM_LIB_PATH}
++incdir+${CASE_PATH}
 
 ${COM_LIB_PATH}/lib_pkg.sv
 ${BENCH_PATH}/bench_pkg.sv
+${BENCH_PATH}/testcase_pkg.sv
---
+++ testbench_lib/testcase/testcase_pkg.sv
@@ -0,0 +1,11 @@
+`ifndef TESTCASE_PKG_SV
+`define TESTCASE_PKG_SV
+
+//include package file
+
+//import packages
+
+//include files
+`include "example_case.sv"
+
+`endif
